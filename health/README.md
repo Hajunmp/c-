2aa64d5fe3c20f9ab895a3bdddaf35c5f83313c6
Hajunmp <nathanielaw88@gmail.com>
2025-02-24 12:30:19 +0700

    healthglow1

---
+++ health/README.md
@@ -0,0 +1,12 @@
+calc bmi
+weight goal
+calorie def/inc
+choose exercise difficulty 1
+38000
+31000
+
+::
+0.75
+0.825
+0.89
+0.88
---
+++ health/healthglow.cpp
@@ -0,0 +1,272 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class BMI
+{
+    public:
+        double height;
+        double weight;
+        int bodymass;
+        void calc()
+        {
+            cout << "Body-Mass index" << endl;
+            cout << "Height (m): ";
+            cin >> height;
+            cout << "Weight (kg): ";
+            cin >> weight;
+            bodymass = weight/pow(height, 2);
+            cout << "Your BMI is : " << bodymass << "kg/m^2" << endl;
+        }
+
+};
+
+class below19 : public BMI
+{
+    public:
+        below19()
+        {
+            calc();
+            if(bodymass<=18.5)
+            {
+                cout << "Underweight" << endl;
+            }
+            else if(bodymass>18.5 && bodymass<=25)
+            {
+                cout << "Normal" << endl;
+            }
+            else if(bodymass>25 && bodymass<=30)
+            {
+                cout << "Overweight" << endl;
+            }
+            else if(bodymass>30 && bodymass<=35)
+            {
+                cout << "Class I Obesity" << endl;
+            }
+            else if(bodymass>35 && bodymass<=40)
+            {
+                cout << "Class II Obesity" << endl;
+            }
+            else if(bodymass>40)
+            {
+                cout << "Class III Obesity" << endl;
+            }
+        }
+};
+
+class below30 : public BMI
+{
+    public:
+        below30()
+        {
+            calc();
+            if(bodymass<=18.5)
+            {
+                cout << "Underweight" << endl;
+            }
+            else if(bodymass>18.5 && bodymass<=24.5)
+            {
+                cout << "Normal" << endl;
+            }
+            else if(bodymass>24.5 && bodymass<=29)
+            {
+                cout << "Overweight" << endl;
+            }
+            else if(bodymass>29 && bodymass<=35)
+            {
+                cout << "Class I Obesity" << endl;
+            }
+            else if(bodymass>35 && bodymass<=40)
+            {
+                cout << "Class II Obesity" << endl;
+            }
+            else if(bodymass>40)
+            {
+                cout << "Class III Obesity" << endl;
+            }
+            
+        }
+};
+
+class below40 : public BMI
+{
+    public:
+        below40()
+        {
+            calc();
+            if(bodymass<=18)
+            {
+                cout << "Underweight" << endl;
+            }
+            else if(bodymass>18 && bodymass<=24)
+            {
+                cout << "Normal" << endl;
+            }
+            else if(bodymass>24 && bodymass<=29)
+            {
+                cout << "Overweight" << endl;
+            }
+            else if(bodymass>29 && bodymass<=34)
+            {
+                cout << "Class I Obesity" << endl;
+            }
+            else if(bodymass>34 && bodymass<=39)
+            {
+                cout << "Class II Obesity" << endl;
+            }
+            else if(bodymass>39)
+            {
+                cout << "Class III Obesity" << endl;
+            }
+            
+        }
+};
+
+class above40 : public BMI
+{
+    public:
+        above40()
+        {
+            calc();
+            if(bodymass<=17)
+            {
+                cout << "Underweight" << endl;
+            }
+            else if(bodymass>17 && bodymass<=24)
+            {
+                cout << "Normal" << endl;
+            }
+            else if(bodymass>24 && bodymass<=28)
+            {
+                cout << "Overweight" << endl;
+            }
+            else if(bodymass>28 && bodymass<=34)
+            {
+                cout << "Class I Obesity" << endl;
+            }
+            else if(bodymass>34 && bodymass<=39)
+            {
+                cout << "Class II Obesity" << endl;
+            }
+            else if(bodymass>39)
+            {
+                cout << "Class III Obesity" << endl;
+            }
+            
+        }
+};
+
+// 1. Harus melangkah (???) step
+// 2. Harus angkat beban (??) KG
+// 3. etc
+
+class goal : public BMI
+{
+    public:
+        int weightgoal;
+        int final;
+        int age;
+        int speed;
+        int duration;
+        int MET;
+        int hours;
+        int burn;
+        int burning;
+        void count_final(double a){
+            cout << "Weight Goal : ";
+            cin >> weightgoal;
+            if(weightgoal>a)
+            {
+                final = weightgoal-a;
+                burning = final*7700;
+                cout << "You need a total of " << burning  << " increase in calories" << endl;
+            }
+            else if(weightgoal<a)
+            {
+                final = a-weightgoal;
+                burning = final*7700;
+                cout << "You need a total of " << burning << " calorie deficit" << endl;
+                cout << "(7700 calories per 1kg of fat)" << endl;
+            }
+        }
+        void exercise(double a){
+
+            while(burning>0)
+            {
+                cout << burning << " calories needed to burn";
+                cout << "choose your running speed" << endl;
+                cout << "1. 8.0 km/h" << endl;
+                cout << "2. 9.7 km/h" << endl;
+                cout << "3. 11.3 km/h" << endl;
+                cout << "4. 12.9 km/h" << endl;
+                cout << "5. 14.5 km/h" << endl;
+                cout << "choice : ";
+                cin >> speed;
+                if (speed == 1)
+                {
+                    MET = 6;
+                }
+                else if(speed==2)
+                {
+                    MET = 8;
+                }
+                else if(speed==3)
+                {
+                    MET = 10;
+                }
+                else if(speed==4)
+                {
+                    MET = 11.5;
+                }
+                else if(speed==5)
+                {
+                    MET = 12.8;
+                }
+                cout << "duration(minutes) : ";
+                cin >> duration;
+                hours = duration/60;
+                burn = MET*a*duration;
+                cout << burn << " calories burnt" << endl;
+                burning -= burn;
+            }
+        }
+        goal()
+        {
+            cout << "Welcome to HealthGlow";
+            cout << "=====================";
+            cout << "Age : ";
+            cin >> age;
+            if(age<=19)
+            {
+                below19 user;
+                count_final(user.weight);  
+                exercise(user.weight);              
+            }
+            else if(age>19 && age<=30)
+            {
+                below30 user;
+                count_final(user.weight); 
+                exercise(user.weight);  
+            }
+            else if(age>30 && age<=40)
+            {
+                below40 user;
+                count_final(user.weight);
+                exercise(user.weight);   
+            }
+            else if(age>40)
+            {
+                above40 user;
+                count_final(user.weight);
+                exercise(user.weight);   
+            }
+        }
+};
+
+int age;
+
+int main()
+{
+    goal user1;
+}
