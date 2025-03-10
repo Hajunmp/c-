f82f2a0a6e43029e51db7776b9297745bae53330
Hajunmp <nathanielaw88@gmail.com>
2025-03-10 12:33:45 +0700

    customeradmin

--- dhl/dhl.cpp
+++ dhl/dhl.cpp
@@ -23,10 +23,19 @@
     class Db
     {
     private:
+    //register
         vector<vector<string>> data_user;
         string username;
         string pass;
         bool verif;
+
+    //package
+        vector<vector<string>> package;
+        string receiver;
+        string address;
+        string object;
+        string weight;
+        string status;
     
     public:
         string bool_to_str(bool a)
@@ -64,40 +73,6 @@
             }
         }
 
-        int admin()
-        {
-            int choose;
-            cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-            cout << "1. Dashboard Package" << endl;
-            cout << "2. User Control" << endl;
-            cout << "3. Exit" << endl;
-            cin >> choose;
-            if (choose==1)
-            {
-
-            }
-            else if (choose==3)
-            {
-                return 0;
-            }
-        }
-        int customer()
-        {
-            int choose;
-            cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-            cout << "1. Send Package" << endl;
-            cout << "2. Status" << endl;
-            cout << "3. Exit" << endl;
-            cin >> choose;
-            if (choose==1)
-            {
-                
-            }
-            else if (choose==3)
-            {
-                return 0;
-            }
-        }
         int check(string a, string b)
         {
             bool exist = false;
@@ -112,6 +87,8 @@
                         {
                             cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
                             cout << "Welcome, " << data_user[i][0] << endl;
+                            username = data_user[i][0];
+                            pass = data_user[i][1];
 
                             if(data_user[i][2]=="true")
                             {
@@ -130,12 +107,211 @@
                 return 2;
             }
         }
+        void set2(string a, string b, string c, string d)
+        {
+            receiver = a;
+            address = b;
+            object = c;
+            weight = d;
+            status = "boarding";
+        }
+        void insert2()
+        {
+            vector<string> row;
+            row.clear();
+            row.push_back(username);
+            row.push_back(pass);
+            row.push_back(receiver);
+            row.push_back(address);
+            row.push_back(object);
+            row.push_back(weight);
+            row.push_back(status);
+            package.push_back(row);
+        }
+        void package_status()
+        {
+            cout << "\033[2J\033[1;1H";
+            bool exist = false;
+            int stop;
+            cout << "Package info = " << "\n";
+            cout << "User -- pass -- receiver -- address -- object -- weight -- Status " << "\n";
+            for (int i = 0; i < package.size(); i++)
+            {
+                if(package[i][0]==username)
+                {   
+                    if(package[i][1]==pass)
+                    {
+                        exist = true;
+                        for (int j = 0; j < 7; j++)
+                        {
+                            cout << "(" << package[i][j] << ")";
+                        }
+                        cout << "\n";
+                    }
+                }
+            }
+            if (exist == false)
+            {
+                cout << "No Packages..." << endl;
+            }
+            cout << "Enter 0 to continue...";
+            cin >> stop;
+            cout << "\033[2J\033[1;1H";
+            return;
+        }
+
+        void dashboard()
+        {
+            int num;
+            int choose;
+            int stop;
+            bool x;
+            while(x=true)
+            {
+                cout << "Package info = " << "\n";
+                cout << "User -- pass -- receiver -- address -- object -- weight -- Status " << "\n";
+                for (int i = 0; i < package.size(); i++)
+                {
+                    cout << i+1 << ". ";
+                    for (int j = 0; j < 7; j++)
+                    {
+                        cout << "(" << package[i][j] << ")";
+                    }
+                    cout << "\n";
+                }
+                cout << "Change user no. ";
+                cin >> num;
+                cout << "\033[2J\033[1;1H";
+                cout << "|========================|" << endl;
+                cout << "|      Update Status     |" << endl;
+                cout << "|========================|" << endl;
+                cout << "| user : " << package[num-1][0] << endl;
+                cout << "| status : " << package[num-1][6] << endl;
+                cout << "|------------------------|" << endl;
+                cout << "| 1. Boarding            |" << endl;
+                cout << "| 2. On the Way          |" << endl;
+                cout << "| 3. Arrived             |" << endl;
+                cout << "| 4. Broken              |" << endl;
+                cout << "|========================|" << endl;
+                cout << "| Choice : ";
+                cin >> choose;
+                if(choose==1)
+                {
+                    package[num-1][6] = "Boarding";   
+                }
+                else if(choose==2)
+                {
+                    package[num-1][6] = "On the Way";   
+                }
+                else if(choose==3)
+                {
+                    package[num-1][6] = "Arrived";   
+                }
+                else if(choose==4)
+                {
+                    package[num-1][6] = "Broken";   
+                }
+                cout << "Status Successfully Updated!" << endl;
+                cout << "|Enter 0 to stop or 1 to continue...";
+                cin >> stop;
+                    
+                if (stop==0){
+                    cout << "\033[2J\033[1;1H";
+                    break;
+                }
+                else{
+                    continue;
+                }
+            }
+        }
     };
 
     class user : public Db
     {
     public : 
-        
+
+        int admin()
+        {
+            int choose;
+            bool x;
+            while(x=true)
+            {
+                cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                cout << "1. Dashboard Package" << endl;
+                cout << "2. User Control" << endl;
+                cout << "3. Exit" << endl;
+                cin >> choose;
+                if (choose==1)
+                {
+                    dashboard();
+                }
+                else if (choose==3)
+                {
+                    return 0;
+                }
+            }
+        }
+        void send_package()
+        {
+            string a;
+            string b;
+            string c;
+            string d;
+            bool x;
+            int stop;
+            while(x=true)
+            {
+                cout << "\033[2J\033[1;1H";
+                cout << "|=======================|" << endl;
+                cout << "|      Send Package     |" << endl;
+                cout << "|=======================|" << endl;
+                cout << "| Receiver : ";
+                cin >> a;
+                cout << "| Address : ";
+                cin >> b;
+                cout << "| Object : ";
+                cin >> c;
+                cout << "| Weight : ";
+                cin >> d;
+                set2(a, b, c, d);
+                insert2();
+                cout << "|Enter 0 to stop or 1 to continue...";
+                cin >> stop;
+                
+                if (stop==0){
+                    cout << "\033[2J\033[1;1H";
+                    break;
+                }
+                else{
+                    continue;
+                }
+            }
+        }
+        int customer()
+        {
+            int choose;
+            bool x;
+            while(x=true)
+            {
+                cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+                cout << "1. Send Package" << endl;
+                cout << "2. Status" << endl;
+                cout << "3. Exit" << endl;
+                cin >> choose;
+                if (choose==1)
+                {
+                    send_package();
+                }
+                else if (choose==2)
+                {
+                    package_status();
+                }
+                else if (choose==3)
+                {
+                    return 0;
+                }
+            }
+        }
     };
     class Regis : public user
     {
